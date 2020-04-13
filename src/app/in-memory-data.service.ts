556bfde5f1d43e9bf363df582fb1d02589c28743
tcs1803047 <tcs1803047@gmail.com>
2020-04-13 10:07:20 +0900

    5 files modified

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,15 +9,15 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 2, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 3, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 4, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 5, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 6, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 7, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 8, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 9, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-      { id: 10, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},                        
+      { id: 2, name: 'Member02', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 3, name: 'Member03', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 4, name: 'Member04', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 5, name: 'Member05', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 6, name: 'Member06', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 7, name: 'Member07', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 8, name: 'Member08', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 9, name: 'Member09', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+      { id: 10, name: 'Member10', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},                        
     ];
     return {heroes};
   }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,15 +2,15 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},];
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
-  { id: 1, name: 'Member01', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},        ]
+  { id: 2, name: 'Member02', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},];
+  { id: 3, name: 'Member03', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 4, name: 'Member04', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 5, name: 'Member05', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 6, name: 'Member06', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 7, name: 'Member07', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 8, name: 'Member08', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 9, name: 'Member09', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},
+  { id: 10, name: 'Member10', mail: 'abc@abc.com', password: '********', petname: 'petname', pettype:'Type A', petbirthday:'2020-01-01'},        ]
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
